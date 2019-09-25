8f7a9f4bb96fa2c0d3e30c3872f0d5fa49e85307
Tabor Shfieraw <shiferawtabor@gmail.com>
2019-09-25 15:47:19 +0300

    fix for remote retrieve

--- vpn/scrapper.py
+++ vpn/scrapper.py
@@ -1,15 +1,15 @@
 import csv 
 from vpn import models 
 import requests
+from io import StringIO
 def scrapVPNS(url):
     print("URL", url)
     headers = {
     'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
     }
-    response = requests.get(url)
-    print("HEREDA", response)
-    print("IA MEHRE")
-    csv_reader = csv.reader(response.text, delimiter=',')
+    response = requests.get('http://localhost/vpndata.csv')
+
+    csv_reader = csv.reader(StringIO(response.text), delimiter=',')
     #csv_reader = csv.reader(ftpstream.read().decode('utf-8'))  # with the appropriate encoding 
 
     line_count = 0
@@ -27,7 +27,6 @@ def scrapVPNS(url):
                 country = models.Country.objects.get(short=country_short)
             except:
                 country = models.Country.objects.create(name=country, short=country_short)
-            print(ping)
             vpn = models.VPN.objects.create(
 
                 hostname = hostname,
@@ -48,5 +47,5 @@ def scrapVPNS(url):
 
             vpn_pks.append( vpn.pk )
         
-    
+    print("VPN COUNT", models.VPN.objects.filter(pk__in = vpn_pks).count())
     models.VPN.objects.exclude(pk__in = vpn_pks).delete()
