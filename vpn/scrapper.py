894acf385c5d32ce1456026724a4eb1adc76a223
Tabor Shfieraw <shiferawtabor@gmail.com>
2019-09-25 14:27:31 +0300

    scrapper fix

--- vpn/scrapper.py
+++ vpn/scrapper.py
@@ -1,51 +1,52 @@
 import csv 
 from vpn import models 
-
+import requests
 def scrapVPNS(url):
-    with open(url) as csv_file:
+    print("URL", url)
+    headers = {
+    'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'
+    }
+    response = requests.get(url)
+    print("HEREDA", response)
+    print("IA MEHRE")
+    csv_reader = csv.reader(response.text, delimiter=',')
+    #csv_reader = csv.reader(ftpstream.read().decode('utf-8'))  # with the appropriate encoding 
+
+    line_count = 0
+    
+    vpn_pks = []
+
+    for row in csv_reader:
+        line_count += 1
+
+        if line_count < 3 or len(row) != 15:
+            continue
+        else:
+            hostname, ip, score, ping, speed, country, country_short, numVpnSessions, uptime, total_users, total_traffic, log_type, operator, message, config_data = row 
+            try:
+                country = models.Country.objects.get(short=country_short)
+            except:
+                country = models.Country.objects.create(name=country, short=country_short)
+            print(ping)
+            vpn = models.VPN.objects.create(
+
+                hostname = hostname,
+                ip = ip,
+                score = score,
+                ping = ping, 
+                speed = speed, 
+                country = country, 
+                numVpnSessions = numVpnSessions,
+                uptime = uptime,
+                totalUsers = total_users,
+                totalTraffic = total_traffic,
+                log_type = log_type,
+                operator = operator,
+                config_data = config_data
+
+            )
+
+            vpn_pks.append( vpn.pk )
         
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        line_count = 0
-        
-        vpn_pks = []
-
-        for row in csv_reader:
-            line_count += 1
-
-            if line_count < 3 or len(row) != 15:
-                print("LEN", len(row))
-                continue
-            else:
-                print("I AM HERES")
-                hostname, ip, score, ping, speed, country, country_short, numVpnSessions, uptime, total_users, total_traffic, log_type, operator, message, config_data = row 
-                try:
-                    country = models.Country.objects.get(short=country_short)
-                except:
-                    country = models.Country.objects.create(name=country, short=country_short)
-                print(ping)
-                vpn = models.VPN.objects.create(
-
-                    hostname = hostname,
-                    ip = ip,
-                    score = score,
-                    ping = ping, 
-                    speed = speed, 
-                    country = country, 
-                    numVpnSessions = numVpnSessions,
-                    uptime = uptime,
-                    totalUsers = total_users,
-                    totalTraffic = total_traffic,
-                    log_type = log_type,
-                    operator = operator,
-                    config_data = config_data
-
-                )
-
-                vpn_pks.append( vpn.pk )
-            
-        
-        models.VPN.objects.exclude(pk__in = vpn_pks).delete()
-
-
-        
-        print(f'Processed {line_count} lines.')
+    
+    models.VPN.objects.exclude(pk__in = vpn_pks).delete()
